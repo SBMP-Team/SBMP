1109cefcfd1edb18d373a21bc127c712679fea2c
Theunderlord9 <jack2011strong@gmail.com>
2026-05-15 19:52:13 +0000

    Updated makefile And Added Temporary Stuff For The Currently Empty Files

---
+++ src/emulator.c
@@ -0,0 +1,7 @@
+#include "lookup.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("SBMP Emulator\n");
+    return 0;
+}
---
+++ src/linker.c
@@ -0,0 +1,7 @@
+#include "lookup.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("SBMP Linker\n");
+    return 0;
+}
