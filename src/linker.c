11d091a1ce356868fec3d26e05746733f16862e4
VolatileCobra77 <nathan.fransham@gmail.com>
2026-05-16 13:41:47 -0400

    Linker mostly working, still some kinks to sort out. Not functional yet

--- src/linker.c
+++ src/linker.c
@@ -1,11 +1,13 @@
 #include "lookup.h"
 #include <stdio.h>
 
-#define MEMORY_OFFSET 0x200
+#define MEMORY_OFFSET 0x0200
+#define PPU_OFFSET 0x8000
 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool read_instruction(FILE *f, uint8_t *opcode, uint16_t *operand) {
     int op = fgetc(f);
@@ -35,14 +37,53 @@ void write_instruction(FILE *f, uint8_t opcode, uint16_t operand) {
 
 int main(int argc, char* argv[]) {
     printf("SBMP Linker\n");
-    FILE *infile = fopen(argv[1], "rb");
-    FILE *outfile = fopen(argv[2], "wb");
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        printf("USAGE: \n -h/--help Prints this help menu and exits.\n -i/--input-file [filepath] Specifies the input .o file\n -p/--ppu-file [filepath] Specifies the input .ppu file\n -o/--output-file [filepath] Specifies the output .sbmp file\nFLAGS:\n --ram-offset [0x0-0xFFFF] This sets the 0x0 register to specify where usable RAM starts\n --rom-offset [0x0-0xFFFF] this sets the 0x1 register to specify where program ROM starts, the usable ram will be between this and the ram offset.\n --ppu-offset [0x0-0xFFFF] Specifies the offset of PPU Memory, this will set the 0x2 register to this value.\n");
+        return 0;
+    }
+    int arg = 0;
+    FILE *infile;
+    FILE *outfile;
+
+    while (argv[arg]!=NULL) {
+        if (argv[arg][0] == '-') {
+            switch (argv[arg][1]) {
+                case '-': {
+                    if (strcmp(argv[arg], "--ram-offset") == 0) {
+                        arg ++;
+
+                    }
+                }
+                case 'i': {
+                    arg++;
+                    infile = fopen(argv[arg], "r");
+                    if (infile == NULL) {
+                        printf("Unable to open input file from %s\n", argv[arg]);
+                    }
+                }
+                case 'o': {
+                    arg++;
+                    outfile = fopen(argv[arg], "w");
+                    if (outfile == NULL) {
+                        printf("Unable to open output file from %s\n", argv[arg]);
+                    }
+                }
+                case 'p': {
+                    arg++;
+                    printf("PPU File loading not implemented");
+                }
+            }
+        }
+    }
+
+
+
 
     if (infile == NULL){
-        printf("Unable to open in file from %s\n", argv[1]);
+        printf("Input file invalid");
     }
     if (outfile == NULL) {
-        printf("Unable to open out file from %s\n", argv[2]);
+        printf("Output file invalid");
     }
     //write zeroes for the first 512 lines, for program memory.
     uint16_t zero = 0;
@@ -53,12 +94,26 @@ int main(int argc, char* argv[]) {
 
     uint8_t opcode = 0;
     uint16_t operand = 0;
+    size_t words = MEMORY_OFFSET;
 
     //read optcode/instruction pairs from the in file and write each to its own line.
     while (read_instruction(infile, &opcode, &operand)) {
         write_instruction(outfile, opcode, operand);
+        words +=2;
+        if (words >= PPU_OFFSET) {
+            perror("More instructions than max, would leak into ppu space. specify --ignore-ppu-leak to ignore this if not using ppu space. specify --ppu-offset to define a new ppu offset if you need more program space.");
+            goto error;
+        }
     }
+    printf("Wrote %lu words to output file, starting ppu section.", words);
 
+
+    fclose(infile);
+    fclose(outfile);
 
     return 0;
+    error:
+    fclose(infile);
+    fclose(outfile);
+    return 1;
 }
