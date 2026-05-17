239a96a73eb64796fa8870a76c7c0cf635440ba8
VolatileCobra77 <nathan.fransham@gmail.com>
2026-05-17 15:51:16 -0400

    Updated Linker

--- src/linker.c
+++ src/linker.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
 bool read_instruction(FILE *f, uint8_t *opcode, uint16_t *operand) {
@@ -38,42 +39,55 @@ void write_instruction(FILE *f, uint8_t opcode, uint16_t operand) {
 int main(int argc, char* argv[]) {
     printf("SBMP Linker\n");
     if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        printf("USAGE: \n -h/--help Prints this help menu and exits.\n -i/--input-file [filepath] Specifies the input .o file\n -p/--ppu-file [filepath] Specifies the input .ppu file\n -o/--output-file [filepath] Specifies the output .sbmp file\nFLAGS:\n --ram-offset [0x0-0xFFFF] This sets the 0x0 register to specify where usable RAM starts\n --rom-offset [0x0-0xFFFF] this sets the 0x1 register to specify where program ROM starts, the usable ram will be between this and the ram offset.\n --ppu-offset [0x0-0xFFFF] Specifies the offset of PPU Memory, this will set the 0x2 register to this value.\n");
+        printf("USAGE: \n -h/--help Prints this help menu and exits.\n -i/--input-file [filepath] Specifies the input .o file\n -p/--ppu-file [filepath] Specifies the input .ppu file\n -o/--output-file [filepath] Specifies the output .sbmp file\nFLAGS:\n --rom-offset [0x0-0xFFFF] this sets the 0x1 register to specify where program ROM starts, the usable ram will be between this and the ram offset.\n --ppu-offset [0x0-0xFFFF] Specifies the offset of PPU Memory, this will set the 0x2 register to this value.\n");
         return 0;
     }
     int arg = 0;
-    FILE *infile;
-    FILE *outfile;
+    FILE *infile = NULL;
+    FILE *outfile = NULL;
+    uint16_t romOffset = MEMORY_OFFSET;
+    uint16_t ppuOffset = PPU_OFFSET;
 
     while (argv[arg]!=NULL) {
         if (argv[arg][0] == '-') {
             switch (argv[arg][1]) {
                 case '-': {
-                    if (strcmp(argv[arg], "--ram-offset") == 0) {
+                    if (strcmp(argv[arg], "--rom-offset") == 0) {
                         arg ++;
-
+                        romOffset = strtol(argv[arg], NULL, 0);
+                        break;
                     }
+                    if (strcmp(argv[arg], "--ppu-offset") == 0) {
+                        arg ++;
+                        ppuOffset = strtol(argv[arg], NULL,0);
+                        break;
+                    }
+                    break;
                 }
                 case 'i': {
                     arg++;
-                    infile = fopen(argv[arg], "r");
+                    infile = fopen(argv[arg], "rb");
                     if (infile == NULL) {
                         printf("Unable to open input file from %s\n", argv[arg]);
                     }
+                    break;
                 }
                 case 'o': {
                     arg++;
-                    outfile = fopen(argv[arg], "w");
+                    outfile = fopen(argv[arg], "wb");
                     if (outfile == NULL) {
                         printf("Unable to open output file from %s\n", argv[arg]);
                     }
+                    break;
                 }
                 case 'p': {
                     arg++;
                     printf("PPU File loading not implemented");
+                    break;
                 }
             }
         }
+        arg ++;
     }
 
 
@@ -87,25 +101,37 @@ int main(int argc, char* argv[]) {
     }
     //write zeroes for the first 512 lines, for program memory.
     uint16_t zero = 0;
-
-    for (int i = 0; i < MEMORY_OFFSET; i++) {
+    uint16_t two = 0x2;
+    fwrite(&two,sizeof(uint16_t),1,outfile);
+    fwrite(&romOffset, sizeof(uint16_t),1,outfile);
+    fwrite(&ppuOffset, sizeof(uint16_t),1,outfile);
+    for (int i = two; i < romOffset; i++) {
         fwrite(&zero, sizeof(uint16_t), 1, outfile);
     }
 
     uint8_t opcode = 0;
     uint16_t operand = 0;
-    size_t words = MEMORY_OFFSET;
+    size_t words = two + romOffset;
 
     //read optcode/instruction pairs from the in file and write each to its own line.
     while (read_instruction(infile, &opcode, &operand)) {
         write_instruction(outfile, opcode, operand);
         words +=2;
-        if (words >= PPU_OFFSET) {
-            perror("More instructions than max, would leak into ppu space. specify --ignore-ppu-leak to ignore this if not using ppu space. specify --ppu-offset to define a new ppu offset if you need more program space.");
+        if (words >= ppuOffset) {
+            perror("More instructions than max, would leak into ppu space. specify --ppu-offset to define a new ppu offset if you need more program space.");
             goto error;
         }
     }
-    printf("Wrote %lu words to output file, starting ppu section.", words);
+    for (size_t i = words; i < ppuOffset; i++) {
+        fwrite(&zero, sizeof(uint16_t), 1, outfile);
+
+    }
+    printf("Wrote %lu words to output file from 0x2 to %hu, starting ppu section.", words, romOffset);
+
+    for (size_t i = ppuOffset; i < 0xFFFF; i++) {
+        fwrite(&zero, sizeof(uint16_t), 1, outfile);
+    }
+
 
 
     fclose(infile);
